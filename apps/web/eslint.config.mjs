ac5191c5de4472ac507a2d7e6e89282a848cde58
Dominik Šelmeci <dominik.selmeci@gmail.com>
2025-10-13 19:46:17 +0200

    fix: linting for monorepo

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,11 +1,10 @@
-import js from "@eslint/js";
-import globals from "globals";
+import base from "@de/config-eslint";
 import prettier from "eslint-config-prettier";
 import reactPlugin from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  js.configs.recommended,
+  ...base,
   {
     ...reactPlugin.configs.flat.recommended,
     settings: {
@@ -17,14 +16,6 @@ export default [
   reactPlugin.configs.flat["jsx-runtime"],
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
     rules: {
       "react/no-unescaped-entities": "off",
       "react/prop-types": "off",
