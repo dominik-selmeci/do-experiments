5825f6416affdb8041496b686341da0f005b303e
Dominik Šelmeci <dominik.selmeci@gmail.com>
2025-10-20 09:03:08 +0200

    feat(web): add form for adding new experiments (UI only)

---
+++ apps/web/src/AddExperiment.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export const AddExperiment = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <form>
+      <fieldset>
+        <legend>Create a new experiment: {name}</legend>
+
+        <div>
+          <label>
+            Name
+            <input
+              name="name"
+              type="text"
+              required
+              maxLength="255"
+              placeholder="e.g., Morning Routine Test"
+              autoComplete="off"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Description
+            <textarea
+              name="description"
+              rows="5"
+              placeholder="tell me more..."
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </label>
+        </div>
+
+        <button type="submit">Create</button>
+      </fieldset>
+    </form>
+  );
+};
--- apps/web/src/app.jsx
+++ apps/web/src/app.jsx
@@ -1,22 +1,32 @@
 import { createRoot } from "react-dom/client";
-import { Experiment } from "./experiment";
+import { Experiment } from "./Experiment";
 import { StrictMode } from "react";
+import { AddExperiment } from "./AddExperiment";
 
 export const App = () => {
   return (
-    <section>
-      <h1>Do experiments!</h1>
-      <Experiment
-        name="Exercice once a day"
-        date="Date: 9.10.2025 - 14.10.2025"
-        routine="Routine: Everytime after breakfast around 10:00"
-      />
-      <Experiment
-        name="Don't use phone till lunch"
-        date="Date: 15.10.2025 - 21.10.2025"
-        routine="Routine: Hide your phone till lunch."
-      />
-    </section>
+    <main className="container">
+      <section>
+        <h1>Do experiments!</h1>
+        <div className="grid">
+          <article>
+            <AddExperiment />
+          </article>
+          <article>
+            <Experiment
+              name="Exercice once a day"
+              date="Date: 9.10.2025 - 14.10.2025"
+              routine="Routine: Everytime after breakfast around 10:00"
+            />
+            <Experiment
+              name="Don't use phone till lunch"
+              date="Date: 15.10.2025 - 21.10.2025"
+              routine="Routine: Hide your phone till lunch."
+            />
+          </article>
+        </div>
+      </section>
+    </main>
   );
 };
 
